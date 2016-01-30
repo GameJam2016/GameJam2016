93b5801ced5d8d9a82a1286179f14b4dd0982f48
Steven Medeot <stermes66@gmail.com>
2016-01-30 01:25:51 -0800

    Created the Menuscreen

---
+++ Assets/Scripts/MenuScreen.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScreen : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_UIText;
+
+    private float m_TextFlashSpeed = 5.0f;
+    void Update()
+    {
+        UpdateText();
+        if (Input.anyKeyDown)
+        {
+            EndMenu();
+        }
+    }
+    
+    void UpdateText()
+    {
+        m_UIText.color = new Color(m_UIText.color.r, m_UIText.color.g, m_UIText.color.b, Mathf.Sin(Time.time * m_TextFlashSpeed));
+    }
+
+    void EndMenu()
+    {
+        Destroy(gameObject);
+    }
+}
