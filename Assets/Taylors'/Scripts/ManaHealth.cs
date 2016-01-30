141f5f329b0887d6633feec015ad048e44f5b387
Taylor <zu.nk.12@hotmail.com>
2016-01-30 17:47:42 -0500

    Commit Stuff

--- Assets/Taylors'/Scripts/ManaHealth.cs
+++ Assets/Taylors'/Scripts/ManaHealth.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class ManaHealth : MonoBehaviour {
-
+public class ManaHealth : MonoBehaviour
+{
+    public Image Mana, Health;
+    public GameObject Player;
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        Health.type = Image.Type.Filled;
+        Health.fillMethod = Image.FillMethod.Horizontal;
+        Health.fillOrigin = (int)Image.OriginHorizontal.Left;
+        Health.fillAmount = 1;
+
+        Mana.type = Image.Type.Filled;
+        Mana.fillMethod = Image.FillMethod.Horizontal;
+        Mana.fillOrigin = (int)Image.OriginHorizontal.Left;
+        Mana.fillAmount = 1;
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void UpdateHealhMana()
+    {
+        Mana.fillAmount = (Player.GetComponent<PlayerStatus>().Mana / Player.GetComponent<PlayerStatus>().Mana);
+        Health.fillAmount = (Player.GetComponent<PlayerStatus>().Health / 100);
+    }
 }
