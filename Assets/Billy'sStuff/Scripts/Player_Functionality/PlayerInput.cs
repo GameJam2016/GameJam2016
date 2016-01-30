27e36612b3fb75ebd670b001c1a70a2aa9d33621
Billy Tan <billy_jacob_tan@hotmail.com>
2016-01-29 22:29:29 -0500

    Added base character functionality

---
+++ Assets/Billy'sStuff/Scripts/Player_Functionality/PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : PlayerFunctionality
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        PlayerInitialize();
+        bIsGrounded = Grounded();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("right"))
+        {
+            MoveRight();
+        }
+
+        else if(Input.GetKey("left"))
+        {
+            MoveLeft();
+        }
+
+        if(Input.GetKey("space"))
+        {
+            Jump();
+        }
+    }
+
+}
