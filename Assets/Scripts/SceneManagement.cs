1cf4ea3120149eca345784a9c04cdd35c1604234
Steven Medeot <stermes66@gmail.com>
2016-01-29 22:24:37 -0800

    Scene Manager implemented Input Manager implemented Audio manager implemented

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the scene using the int that represents it in the build
+    /// </summary>
+    /// <param name="sceneNum">Number of the scene</param>
+    public static void LoadScene(int sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+
+    /// <summary>
+    /// Loads the scene using the name of the scene in the build
+    /// </summary>
+    /// <param name="sceneName">scene name</param>
+    public static void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
